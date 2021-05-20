f8d5f3b73cf04fad822e2c9d8f92d61ca0b4aa8b
Cecilia Lobo <cecilialobo92@gmail.com>
2021-05-20 13:47:30 +0200

    Delete Inspect component and create validationChallenge1()

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import './App.css';
-import Inspect from './components/Inspect';
 import Introduction from './components/Introduction';
 import Password from './components/Password';
 import Vault from './components/Vault';
 
 function App() {
 
-  const changeDisplay = (idToNone, idToBlock ) => {
+  const changeDisplay = (idToNone, idToBlock) => {
     document.getElementById(idToNone).style.display = 'none';
     document.getElementById(idToBlock).style.display = 'block';
+  }
+
+  const validationChallenge1 = () => {
+    const alarmCode = document.getElementById('inspect-input').value;
+    if (alarmCode === 'ALIENS_ROCK!!') {
+      changeDisplay('challenge1', 'challenge2')
+    } else {
+      alert('Sorry, you have the wrong code. Alarm not disabled!')
+    }
   }
 
   return (
     <>
       <div id='intro'>
         <Introduction />
-        <button id='introButton' onClick={() => {changeDisplay('intro', 'challenge1')}}>Let's do it!</button>
+        <button id='intro-button' onClick={() => {changeDisplay('intro', 'challenge1')}}>Let's do it!</button>
       </div>
       
       <div id='challenge1'>
-        <Inspect />
-        <button onClick={() => {changeDisplay('challenge1', 'challenge2')}}>DISABLE ALARM</button>
+        <p id='inspect-p'>
+          First, you need to find the code to disable the alarm of the vault. To find the alarm's code, you need to Hack and <strong>inspect</strong> the hell out of this vault! Find the code and type below.
+        </p>
+        <label id='inspect-label' for='inspect-code'>Type here the alarm code:</label>
+        <input id='inspect-input' type='text'></input>
+        <button onClick={() => {validationChallenge1()}}>DISABLE ALARM</button>
       </div>
 
       <div id='challenge2'>
