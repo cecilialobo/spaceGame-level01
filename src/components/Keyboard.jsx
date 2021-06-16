0d0694052c2cc4070de6a990381832d1dea937c7
Cecilia Lobo <cecilialobo92@gmail.com>
2021-06-16 21:58:42 +0200

    Rename function and identation on Keyboard component.

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from './Button/Button';
 
 const Keyboard = () => {
@@ -9,7 +9,7 @@ const Keyboard = () => {
         setPassword(password + number);
     }
 
-    const validationChallenge3 = (password) => {
+    const validateChallenge3 = (password) => {
         if (password === '134076') {
             alert('CONGRATS! You hacked the vault and recovered the lost GPS! Press ok to move forward to the next level and find more spaceship pieces!');
             window.parent.postMessage('nextLevel');
@@ -22,25 +22,26 @@ const Keyboard = () => {
     return (
         <>
             <div className='btn-container'>
-                <Button number='1' typedNumber={updatePassword}/>
-                <Button number='2' typedNumber={updatePassword}/>
-                <Button number='3' typedNumber={updatePassword}/>
+                <Button number='1' typedNumber={updatePassword} />
+                <Button number='2' typedNumber={updatePassword} />
+                <Button number='3' typedNumber={updatePassword} />
             </div>
 
             <div className='btn-container'>
-                <Button number='4' typedNumber={updatePassword}/>
-                <Button number='5' typedNumber={updatePassword}/>
-                <Button number='6' typedNumber={updatePassword}/>
+                <Button number='4' typedNumber={updatePassword} />
+                <Button number='5' typedNumber={updatePassword} />
+                <Button number='6' typedNumber={updatePassword} />
             </div>
 
             <div className='btn-container'>
-                <Button number='7' typedNumber={updatePassword}/>
-                <Button number='8' typedNumber={updatePassword}/>
-                <Button number='9' typedNumber={updatePassword}/>
+                <Button number='7' typedNumber={updatePassword} />
+                <Button number='8' typedNumber={updatePassword} />
+                <Button number='9' typedNumber={updatePassword} />
             </div>
-            <Button number='0' typedNumber={updatePassword}/>
-            <button className='button' onClick={()=> {
-                validationChallenge3(password)}}>OPEN THE VAULT</button>
+            <Button number='0' typedNumber={updatePassword} />
+            <button className='button' onClick={() => {
+                validateChallenge3(password)
+            }}>OPEN THE VAULT</button>
         </>
     )
 }
